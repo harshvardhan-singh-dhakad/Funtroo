78e75c18e2ecff5e77e90f18fb928acaa717d289
Harshvardhan Singh Dhakad <harshvardhan@funtroo.in>
2026-08-23 19:35:54 +0530

    Fix NEXTAUTH_URL by making it dynamically detect the request origin

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,14 @@
-import NextAuth from 'next-auth'
-import { authOptions } from '@/lib/auth'
+export const dynamic = 'force-dynamic';
 
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+import { NextRequest } from 'next/server';
+import NextAuth from 'next-auth';
+import { authOptions } from '@/lib/auth';
+
+async function auth(req: NextRequest, ctx: any) {
+  if (process.env.NODE_ENV === 'production') {
+    process.env.NEXTAUTH_URL = req.nextUrl.origin;
+  }
+  return NextAuth(authOptions)(req, ctx);
+}
+
+export { auth as GET, auth as POST };
